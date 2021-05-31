068b4812380c70747051e09882b1227decde9bc6
jazmin <tegan01sara12@gmail.com>
2021-05-31 18:19:07 -0500

    l

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,50 @@
 const cipher = {
   // ...
+  encode (offset, text){
+    
+    let cifradoFinal = "";
+    let displacement = Number(offset);
+    if (text!=null){
+      for (let i = 0; i<text.length; i++){
+      let ascii = text.charCodeAt(i);
+      let formula = (((ascii - 65 + displacement) % 26) + 65);
+       cifradoFinal+= String.fromCharCode(formula);
+      
+      } 
+   
+  
+    } 
+        
+    return cifradoFinal;
+  
+  },
+
+    decode (offset2, text2) {
+    let cifradoFinal2 = "";
+    let displacement2 = Number(offset2);
+    if (text2!=null){
+    for (let i = 0; i<text2.length; i++){
+      let ascii2 = text2.charCodeAt(i);
+      let formula2 = (((ascii2 + 65 - displacement2) % 26) + 65);
+
+      cifradoFinal2+= String.fromCharCode(formula2);
+
+      }
+    }
+
+    return cifradoFinal2;
+
+
+  }
+
 };
+
+
+     
+    
+
+
+
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+//import { SourceCode } from 'eslint';
 import cipher from './cipher.js';
+
+document.getElementById("buttonCode").addEventListener("click", function(){
+    
+    let text = document.getElementById("userText").value;
+   
+    let offset= document.getElementById("desplazamiento").value;
+    
+    document.getElementById("textcode").value = cipher.encode(offset,text);
+  
+
+
+
+
+
+});
+
+document.getElementById("buttonDecode").addEventListener("click", function(){
+    
+  let text2 = document.getElementById("userText").value;
+ 
+  let offset2= document.getElementById("desplazamiento").value;
+  
+  document.getElementById("textcode").value = cipher.decode(offset2,text2);
+
+
+
+
+});
+
+
+
+   // let cifrado = document.querySelector("encode");text
+ //}
 
 console.log(cipher);
